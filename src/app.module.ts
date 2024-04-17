5e065f9927ca4c9d7950554735de40e8b9dd28d4
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-16 21:41:31 -0500

    Clean project, changes initial assistant config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 import { ChatModule } from './chat/chat.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     ChatModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/chat/initial-config.ts
+++ src/chat/initial-config.ts
@@ -1,12 +1,26 @@
 import { Message, Role } from "./interfaces/request-body.interface";
 
+const currentDate = new Date();
+
 export const configMessage: Message[] = [
     {
         role: Role.System,
-        content: "Answer in Spanish and English."
+        content: "Eres el asistente virtual de Canchas San Marcos.",
     },
     {
         role: Role.System,
-        content: "There is a bussines that rent sport courts; currently, there are three available dates to rent a court: april 17th, april 22nd, and may 1st. You are an assistant who helps with the schedule of the business.",
+        content: "Canchas San Marcos alquila canchas de futbol, de voleibol y de basquetbol.",
+    },
+    {
+        role: Role.System,
+        content: `El cliente debe seleccionar una fecha entre el ${currentDate.getDate()} de abril y el 30 de abril`,
+    },
+    {
+        role: Role.System,
+        content: "El cliente debe seleccionar un horario. Los horarios disponibles son de 9:00 a 12:00, y de 14:00 a 20:00."
+    },
+    {
+        role: Role.System,
+        content: "Una vez el cliente seleccione una fecha y un horario, debe brindar su nombre, numero telefonico y correo para confirmar.",
     },
 ]
